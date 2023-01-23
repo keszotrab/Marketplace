e68084dca63900a6b796d09459e40186bc8577c2
Roman Dmowski <mistrzugames@gmail.com>
2023-01-23 15:04:44 +0100

    Dodanie rozsuwanego menu

--- Marketplace/Marketplace/Program.cs
+++ Marketplace/Marketplace/Program.cs
@@ -13,7 +13,7 @@ namespace Marketplace
 
             // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
-            {
+            { 
                 app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
